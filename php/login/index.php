05cdbc2fab490d2ef5efc17947dce43c6a1a6541
Grazziano Borges Fagundes <grazzianofagundes@gmail.com>
2019-09-09 11:39:10 -0300

    Formulário de login

--- php/login/index.php
+++ php/login/index.php
@@ -1,3 +1,28 @@
 <?php
+?>
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<link rel="stylesheet" type="text/css" href="../../css/formLogin.css">
 
-echo "Hello World!";
+<div class="container">
+	<div class="card card-container">
+		<img id="profile-img" class="profile-img-card" src="//ssl.gstatic.com/accounts/ui/avatar_2x.png" />
+		<p id="profile-name" class="profile-name-card"></p>
+		<form class="form-signin">
+			<span id="reauth-email" class="reauth-email"></span>
+			<input type="email" id="email" class="form-control" placeholder="E-mail" required autofocus>
+			<input type="password" id="senha" class="form-control" placeholder="Senha" required>
+			<!-- <div id="remember" class="checkbox">
+				<label>
+					<input type="checkbox" value="remember-me"> Remember me
+				</label>
+			</div> -->
+			<button class="btn btn-lg btn-primary btn-block btn-signin" type="submit">Entrar</button>
+		</form>
+
+		<a href="#" class="forgot-password">
+			Forgot the password?
+		</a>
+	</div>
+</div>
